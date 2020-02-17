b564d181e15fb32d381000e6649acc72218491ca
Lisa Søndergaard <lisa3074@stud.kea.dk>
2020-02-17 11:53:43 +0100

    Before peters halløj

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let filter = "Alle";
-let SortButtons = document.querySelectorAll(".filter");
-const jsonUrl = "https://petlatkea.dk/2020/hogwarts/students.json";
-let student;
+//let HTML.filter = "Alle";
 
-window.addEventListener("DOMContentLoaded", start);
-const allStudents = [student];
+const HTML = {};
+//let HTML.SortButtons = document.querySelectorAll(".filter");
+//const HTML.jsonUrl = "https://petlatkea.dk/2020/hogwarts/students.json";
+let studentObject;
+
+window.addEventListener("DOMContentLoaded", init);
+const allStudents = [studentObject];
 const Student = {
   firstName: "",
   middelName: "",
@@ -14,12 +16,18 @@ const Student = {
   theHouse: "",
   fullName: ""
 };
+function init() {
+  HTML.jsonUrl = "https://petlatkea.dk/2020/hogwarts/students.json";
+  HTML.sortButtons = document.querySelectorAll(".filter");
+  HTML.filter = "Alle";
+  start();
+}
 
 function start() {
   console.log("start");
   document.querySelector(".popup").classList = "popup hide";
   document.querySelector(".alle").classList.add("chosen");
-  SortButtons.forEach(element => {
+  HTML.sortButtons.forEach(element => {
     element.addEventListener("click", sort);
   });
   document.querySelector("select").addEventListener("change", function() {
@@ -31,101 +39,16 @@ function start() {
 
 async function fetchJson() {
   console.log("fetchJson");
-  let response = await fetch(jsonUrl);
-  houses = await response.json();
-  prepareObjects(houses);
+  let response = await fetch(HTML.jsonUrl);
+  jsonEntries = await response.json();
+  makeObjects(jsonEntries);
+  fetchHouses();
 }
 
-function fetchHouses(student) {
-  console.log("fetchHouses");
-  document.querySelector("#student_list").innerHTML = "";
-
-  allStudents.forEach(() => {
-    if (filter == student.theHouse || filter == "Alle") {
-      const clone = document.querySelector(".temp").content.cloneNode(true);
-      clone.querySelector(".house").textContent += student.theHouse;
-      clone.querySelector(".name").textContent += student.fullName;
-      clone.firstElementChild.addEventListener("click", function() {
-        popUp(student);
-      });
-      document.querySelector("#student_list").appendChild(clone);
-    }
-  });
-}
-
-/* function fetchHouses(student) {
-  // clear the list
-  document.querySelector("#student_list").innerHTML = "";
-
-  // build a new list
-  allStudents.forEach(displayStudent(student));
-}
-
-function displayStudent(student) {
-  console.log("displayStudent");
-  console.log(student);
-  // create clone
-  const clone = document.querySelector(".temp").content.cloneNode(true);
-
-  // set clone data
-  clone.querySelector(".house").textContent += student.theHouse;
-  clone.querySelector(".name").textContent += student.fullName;
-
-  // append clone to list
-  document.querySelector("#student_list").appendChild(clone);
-} */
-
-function sort() {
-  console.log("sort");
-  SortButtons.forEach(button => {
-    button.classList.remove("chosen");
-  });
-  this.classList.add("chosen");
-  filter = this.dataset.kategori;
-  fetchJson();
-}
-function selectSort() {
-  document.querySelector("select").addEventListener("change", function() {
-    filter = event.target.value;
-    fetchJson();
-  });
-}
-
-function popUp() {
-  console.log("popUp");
-  if ((student.middleName = "")) {
-    document.querySelector(".pop_name").textContent = `${student.firstName} ${student.lastName}`;
-  }
-  document.querySelector(".popup").classList = "popup";
-  document.querySelector(".pop_name").textContent = `${student.firstName} ${student.middleName} ${student.lastName}`;
-  document.querySelector(".pop_nick").textContent = student.nickName;
-  document.querySelector(".pop_gender").textContent = student.gender;
-  document.querySelector(".crest").src = "billeder/" + student.theHouse + ".png";
-  document.querySelector(".crest").alt = student.theHouse + " crest";
-  document.querySelector(".pop_house").textContent = student.theHouse;
-  //theme changes in relation to house in the json file
-  document.querySelector(".popup").dataset.theme = student.theHouse;
-  const dataHouse = document.querySelector(".popup").dataset.house;
-}
-
-/* student.fullName = trimmedNames;
-student.firstName = trimmedFirst;
-student.lastName = trimmedLast;
-student.gender = trimmedGender;
-student.theHouse = trimmedHouse;
-  student.nickName = nick;
-  student.middelName = trimmedMiddel;*/
-
-function close() {
-  document.querySelector(".close").addEventListener("click", function() {
-    document.querySelector(".popup").classList = "popup hide";
-  });
-}
-
-function prepareObjects(houses) {
-  houses.forEach(jsonStudent => {
+function makeObjects(jsonEntries) {
+  jsonEntries.forEach(jsonStudent => {
     //console.log(jsonStudent);
-    student = Object.create(Student);
+    studentObject = Object.create(Student);
 
     //Raw json data
     //The whole string
@@ -180,22 +103,80 @@ function prepareObjects(houses) {
     const nick = trimmedMiddel.substring(findSpecialCharacters + 1, lastSpace);
 
     //Send to object
-    student.fullName = trimmedNames;
-    student.firstName = trimmedFirst;
-    student.lastName = trimmedLast;
-    student.gender = trimmedGender;
-    student.theHouse = trimmedHouse;
+    studentObject.fullName = trimmedNames;
+    studentObject.firstName = trimmedFirst;
+    studentObject.lastName = trimmedLast;
+    studentObject.gender = trimmedGender;
+    studentObject.theHouse = trimmedHouse;
     const find = trimmedMiddel.substring(0, 1);
     if (find === '"' || find === "'") {
-      student.nickName = nick;
+      studentObject.nickName = nick;
       console.log("Nickname");
     } else {
-      student.middelName = trimmedMiddel;
+      studentObject.middelName = trimmedMiddel;
       console.log("Middlename");
     }
 
-    console.log(student);
-    allStudents.push(student);
+    allStudents.push(studentObject);
+    console.log(studentObject);
   });
-  fetchHouses(student);
+}
+
+function fetchHouses() {
+  console.log("fetchHouses");
+  console.log(studentObject);
+  console.log(allStudents);
+  document.querySelector("#student_list").innerHTML = "";
+
+  allStudents.forEach(() => {
+    console.log("test");
+    if (HTML.filter == studentObject.theHouse || HTML.filter == "Alle") {
+      const clone = document.querySelector(".temp").content.cloneNode(true);
+      clone.querySelector(".house").textContent += studentObject.theHouse;
+      clone.querySelector(".name").textContent += studentObject.fullName;
+      clone.firstElementChild.addEventListener("click", function() {
+        popUp(studentObject);
+      });
+      document.querySelector("#student_list").appendChild(clone);
+    }
+  });
+}
+
+function sort() {
+  console.log("sort");
+  HTML.SortButtons.forEach(button => {
+    button.classList.remove("chosen");
+  });
+  this.classList.add("chosen");
+  HTML.filter = this.dataset.kategori;
+  fetchJson();
+}
+function selectSort() {
+  document.querySelector("select").addEventListener("change", function() {
+    HTML.filter = event.target.value;
+    fetchJson();
+  });
+}
+
+function popUp() {
+  console.log("popUp");
+  if ((studentObject.middleName = "")) {
+    document.querySelector(".pop_name").textContent = `${student.firstName} ${student.lastName}`;
+  }
+  document.querySelector(".popup").classList = "popup";
+  document.querySelector(".pop_name").textContent = `${studentObject.firstName} ${studentObject.middleName} ${studentObject.lastName}`;
+  document.querySelector(".pop_nick").textContent = studentObject.nickName;
+  document.querySelector(".pop_gender").textContent = studentObject.gender;
+  document.querySelector(".crest").src = "billeder/" + studentObject.theHouse + ".png";
+  document.querySelector(".crest").alt = studentObject.theHouse + " crest";
+  document.querySelector(".pop_house").textContent = studentObject.theHouse;
+  //theme changes in relation to house in the json file
+  document.querySelector(".popup").dataset.theme = studentObject.theHouse;
+  const dataHouse = document.querySelector(".popup").dataset.house;
+}
+
+function close() {
+  document.querySelector(".close").addEventListener("click", function() {
+    document.querySelector(".popup").classList = "popup hide";
+  });
 }
